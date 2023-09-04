7429f57a359ee7fedf8352af63d0f86751d68148
LuYF-Lemon-love <3555028709@qq.com>
2023-09-04 13:15:47 +0800

    修改微调脚本

--- get_start.py
+++ get_start.py
@@ -20,3 +20,19 @@ generate_input = {
 generate_ids  = model.generate(**generate_input)
 text = tokenizer.decode(generate_ids[0])
 print(text)
+input_ids = tokenizer(['Human: 介绍一下中国\nAssistant: '], return_tensors="pt",add_special_tokens=False).input_ids.to('cuda')        
+generate_input = {
+    "input_ids":input_ids,
+    "max_new_tokens":512,
+    "do_sample":True,
+    "top_k":50,
+    "top_p":0.95,
+    "temperature":0.3,
+    "repetition_penalty":1.3,
+    "eos_token_id":tokenizer.eos_token_id,
+    "bos_token_id":tokenizer.bos_token_id,
+    "pad_token_id":tokenizer.pad_token_id
+}
+generate_ids  = model.generate(**generate_input)
+text = tokenizer.decode(generate_ids[0])
+print(text)
